974d3266c8cc7bb18a7d578d349febbd5f0a1f85
Linda-Lou-A <linda.barker7@hotmail.com>
2025-10-26 16:27:12 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: [
+    './*.html', // Looks for HTML files directly in the root (like index.html)
+    './src/**/*.{html,js}', // Adjust if your source code is elsewhere
+  ],
   theme: {
     extend: {},
   },
