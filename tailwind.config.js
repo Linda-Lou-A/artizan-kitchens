4ee8f5f8b637e3d3e056c47847107e61da6bcb93
Linda-Lou-A <linda.barker7@hotmail.com>
2025-10-26 16:38:28 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,13 @@
 module.exports = {
   content: [
     './*.html', // Looks for HTML files directly in the root (like index.html)
-    './src/**/*.{html,js}', // Adjust if your source code is elsewhere
+    './src/**/*.{html,js}', // You can adjust this if your HTML/JS is in a different structure
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
 
